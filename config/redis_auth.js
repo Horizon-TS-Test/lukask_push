0a6f5e95fc261ce99364b3d0798755f793e4d496
Dennys Ivan Moyón Gunsha <36491360+dennysmoyon@users.noreply.github.com>
2018-08-08 01:45:38 -0500

    Update redis_auth.js

--- config/redis_auth.js
+++ config/redis_auth.js
@@ -1,5 +1,6 @@
 module.exports = {
-    host: 'https://www.lukaksarticles.com',
+    //host: 'https://www.lukaksarticles.com',
+    host: 'https://18.288.114.178',
     port: 6379,
     password: 'sm@rtest7!'
 }
